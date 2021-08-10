7af35ef01714ecbec687e665ef53bccdda3969e4
jamesburgess44 <jamesburgess44@yahoo.com>
2021-08-10 15:15:33 -0600

    debugged shoppingCart Model

--- eCommerceStarterCode/Models/ShoppingCart.cs
+++ eCommerceStarterCode/Models/ShoppingCart.cs
@@ -8,7 +8,7 @@ namespace eCommerceStarterCode.Models
     public class ShoppingCart
     {
        // public int ShoppingCartId { get; set; }
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public User User { get; set; }
         public int MerchId { get; set; }
         public Merch Merch { get; set; }
