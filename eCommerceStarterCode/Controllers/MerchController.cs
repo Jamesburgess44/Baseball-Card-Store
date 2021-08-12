6fc8aac3c0db646c4f16d6b6610e8b3c0785631b
jamesburgess44 <jamesburgess44@yahoo.com>
2021-08-12 09:10:43 -0600

    Pushing for Kirk, added method for get all merch and tested with Postman.

--- eCommerceStarterCode/Controllers/MerchController.cs
+++ eCommerceStarterCode/Controllers/MerchController.cs
@@ -20,6 +20,14 @@ namespace eCommerceStarterCode.Controllers
             _context = context;
         }
         //Start requests here.
+        // get all merch 
+        [HttpGet]
+        public IActionResult GetAllMerch()
+        {
+            var merches = _context.Merches;
+            return Ok(merches);
+        }
+        // get merch by id TESTED
         [HttpGet("{id}")]
         public IActionResult GetMerchById(int id)
         {
@@ -30,7 +38,7 @@ namespace eCommerceStarterCode.Controllers
             }
             return Ok(merch);
         }
-
+        // post or "add" new merch to database TESTED
         [HttpPost]
         public IActionResult Post([FromBody] Merch value)
         {
@@ -38,7 +46,7 @@ namespace eCommerceStarterCode.Controllers
             _context.SaveChanges();
             return StatusCode(201, value);
         }
-
+        // update merch by id TESTED
         [HttpPatch("{id}")]
         public IActionResult Put(int id, [FromBody] Merch value)
         {
@@ -48,6 +56,7 @@ namespace eCommerceStarterCode.Controllers
             _context.SaveChanges();
             return Ok(merch);
         }
+        // delete merch by id TESTED 
         [HttpDelete("{id}")]
         public IActionResult Remove (int id )
         {
